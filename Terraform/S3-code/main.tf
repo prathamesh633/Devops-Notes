1f622fefab37e9770e70a8757e4e53da298a8f3c
prathamesh633 <prathmeshbhujade1234@gmail.com>
2025-02-14 19:20:24 +0530

    deleated Terraform folder

--- Terraform/S3-code/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "buckets" { # Creates 5 identical resources
-  bucket = var.bucketname  # Unique name for each bucket
-}
-  # Optional additional configuration
-#   tags = {
-#     Environment = "Test"
-#   }
-# }
